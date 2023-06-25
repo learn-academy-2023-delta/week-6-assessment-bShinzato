e1389625396858471bc44216848e20f66a317266
bShinzato <bshinzato@outlook.com>
2023-06-25 16:39:37 -0700

    finish week 6

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,39 +14,135 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
-const hitchhikersCharacters = [
-  { name: "ford prefect", occupation: "a hitchhiker" },
-  { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "a radio employee" }
-]
-// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+describe("sentenceWithFirstLastCapitalized", () => {
+  it("returns an array with a sentence about each person with their name capitalized", () => {
+    const hitchhikersCharacters = [
+      { name: "ford prefect", occupation: "a hitchhiker" },
+      { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+      { name: "arthur dent", occupation: "a radio employee" }
+    ]
+    // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+    expect(sentenceWithFirstLastCapitalized(hitchhikersCharacters)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+  })
+})
+
+// ReferenceError: sentenceWithCaps is not defined
 
 // b) Create the function that makes the test pass.
 
+// Create function. One parameter.
+const sentenceWithFirstLastCapitalized = (arrayOfObjects) => {
+  // Declare new variable. Use HOF .map to iterate over arrayOfObjects.
+  const firstLastNameCapital = arrayOfObjects.map(value => {
+    // Object Destructuring. Use Dot notation for the value of name. Use Built-in Method .split(" "), use space as a delimiter.
+    // Built-in Method will return an array with a length of two. 
+    // Variable firstName is set to the first element in the array. Variable lastName is set to the second element in the array.
+    [firstName, lastName] = value.name.split(" ")
+  // Delcare new variable. Call upon our destructured variable firstName.
+  // Use Built-in Methods .charAt(), .toUpperCase() to indicate the first index.
+  // Use arithmetic Operator +, and Built-in Method .substring(1) to extract characters from our starting index of (1). Returns a substring.
+  const firstNameUpperCase = firstName.charAt(0).toUpperCase() + firstName.substring(1)
+  // Delcare new variable. Call upon our destructured variable lastName.
+  // Use Built-in Methods .charAt(), .toUpperCase() to indicate the first index.
+  // Use arithmetic Operator +, and Built-in Method .substring(1) to extract characters from our starting index of (1). Returns a substring.
+  const lastNameUpperCase = lastName.charAt(0).toUpperCase() + lastName.substring(1)
+  // Declare a new variable. Use dot notation to call upon the occupation.
+  const addOcuppation = value.occupation
+  // Return a String Interpolation with firstNameUpperCase, lastNameUpperCase, and addOcuppation. Add "is" and "." to match expected outcome.
+  return `${firstNameUpperCase} ${lastNameUpperCase} is ${addOcuppation}.`
+  })
+  // Return firstLastNameCapital outside of the HOF .map.
+  return firstLastNameCapital
+}
+
+
 // Pseudo code:
+// Create a function called sentenceWithFirstLastCapitalized. Takes one parameter called arrayOfObjects.
+// Declare a new variable named firstLastNameCapital
+// Use HOF .map to iterate on our arrayOfObjects with one parameter called value.
+// We call upon object destructuring with variable names firstName and lastName. Since it is a object use dot notation to call upon .name, use built in method .split(" "). Thus will specify our first element using .split(" ") to firstName and our second element to lastName.
+// Declare a new variable named firstNameUpperCase. 
+    // Call upon firstName with built in methods .charAt() and .toUpperCase() * can use these because it is a string
+    // Use arithmetic operator and built in method .substring(1) to return a substring of the original string.
+// declare a new variable named lastNameUpperCase and do the same thing but change firstName to lastName.
+// declare a new variable called addOcuppation.
+    // Since it is an object we use dot notation to pull the value from the key .occupation.
+// return a string interpolation for firstNameUpperCase, lastNameUpperCase and addOccupation. Look at the expected output and insert "is" and a "." within the string interpolation.
+// return firstLastNameCapital outside of the HOF block but within setenceWithFirstLastCapitalized function.
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
-// Expected output: [ 2, 0, -1, 0 ]
-const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
-// Expected output: [ 2, 1, -1 ]
+describe("onlyRemainders", () => {
+  it("returns an array of only the REMAINDERS of the numbers when divided by 3.", () => {
+    const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+    // Expected output: [ 2, 0, -1, 0 ]
+    const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+    // Expected output: [ 2, 1, -1 ]
+    
+    expect(onlyRemainders(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    expect(onlyRemainders(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
+
+// ReferenceError: onlyRemainders is not defined
 
 // b) Create the function that makes the test pass.
 
+const onlyRemainders = (param) => {
+  // Use HOF .filter() to iterate and filter out everything that is not a number.
+  return param.filter(value => typeof value  === "number")
+  // use HOF .map() to interate each value with modulo by 3 to get the remainders.
+  .map(value => value % 3)
+}
+
 // Pseudo code:
+// Create a function called onlyRemainders. Takes 1 parameter.
+// Use HOF .filter to iterate over array to check for only Numbers.
+// use built-in method typeof, strict equailty operator, and 'number' to specify the datatype.
+// use .map and modulo 3 to get the remainders.
+
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const cubeAndSum1 = [2, 3, 4]
-// Expected output: 99
-const cubeAndSum2 = [0, 5, 10]
-// Expected output: 1125
+describe("sumOfCubed", () => {
+  it("returns the sum of all the numbers cubed.", () => {
+    const cubeAndSum1 = [2, 3, 4]
+    // Expected output: 99
+    const cubeAndSum2 = [0, 5, 10]
+    // Expected output: 1125
+
+    expect(sumOfCubed(cubeAndSum1)).toEqual(99)
+    expect(sumOfCubed(cubeAndSum2)).toEqual(1125)
+  })
+})
+
 
 // b) Create the function that makes the test pass.
+const sumOfCubed = (param) => {
+  // declare a new variable, use HOF .map() to iterate on parameter, use ** to indicate a exponent.
+  let cubedOfNums = param.map(value => value **3)
+  // use HOF .reduce() to iterate over cubedOfNums to accumlate a single value. Using two params.
+  return cubedOfNums.reduce((acc, currentValue) => 
+  // add the two parameters with the our initial value being 0.
+    acc + currentValue, 0
+  )
+}
+
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
+
 
 // Pseudo code:
+// create a function called sumOfCubed. takes 1 parameter called param.
+// declare a new variable named cubedOfNums
+// use HOF .map on our parameter to iterate over the array.
+// use arithmetic operator ** to specify exponent.
+// use HOF .reduce() to iterate over cubedOfNums to accumulate a single value. using two params.
+// use arethic operator plus to get the sum of all the numbers.
+// indicate our initial value to be 0.
--- interview-questions.md
+++ interview-questions.md
@@ -8,36 +8,58 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. As a developer, I am creating a Rails application with a model called Cohort and a model called Student. The cohort model has_many students, but OOPS! I forgot to add the foreign key. How can I fix this mistake? What is the name of the foreign key? Would the foreign key be on the Cohort model or the Student model?
 
-Your answer:
+Your answer: To fix this mistake, generate a migrations file named specifically what your intentions are to do to. After generating the migrations file, need to run $rails db: migrate which creates a new file. Within that file and within that method, use add_column :table_name, :column_name, :datatype. :table_name specifies which Model you are targeting, :column_name correlates with what you want to add, since it is missing a foreign key, it will be :cohort_id. This will be on the student model.
 
-Researched answer:
+Researched answer: To fix this mistake, open the terminal make sure you are in the correct file and run command $rails g migration, name the file name that starts with the action that describes the purpose of the migration i.e add_column_to_list. Afterwards, a file will be added to the migrate directory and within that file there is a def change method already built. Next, within that method, we want to add a column, add_column :table_name, :column_name, :datatype. :table_name specifies which table we are adding too (:students), :column_name is the name of the new column (:cohort_id), and :datatype is a primitive data type (integer). Since the cohort model has_many students the foreign key will be :cohort_id because it is specifying students belongs_to cohort. The foreign key will also be on the Student model. After checking the migrations file, if it looks correct run db:migrate then it will show up on the schema file.
 
 2. Which RESTful routes must always be passed params? Why?
 
-Your answer:
+Your answer: The RESTful routes that must take a parameter is show, edit, update, destroy. For all of these RESTful routes the param is needed in order to specify which primary key or ID is needed to specify in the url. 
 
-Researched answer:
+Researched answer: The RESTful routes that must take a parameter are show, edit, create, update, and destroy. Within the Show method, the param is used to show one item in the database by accessing the ID. Edit, will use a form for the specified id you want to edit. Create will call upon the strong params method. Update will update the specified item in the database by accessing the ID. Destroy will delete the specified item in the database by accessing the ID.
 
 3. Name three rails generator commands. What is created by each?
 
-Your answer:
+Your answer: The three rails generator commands are model, conroller, and resource. Model creates a database. Controller creates the controller file for the model. Resource creates the model, controller, routes, and view files. 
 
-Researched answer:
+Researched answer: The three rails generator commands are model, controller, and resource. When generating a model, it creates a model, migration, and rspec file. When generating a controller, it creates a controller,views, and rpsec file. When generating resource, it generates similar to model, in addition it creates model, controller,views,routes, and respective rspec files.
 
 4. Consider the Rails routes below. What is the name of the controller method that would be called by each route? What action would each of the controller methods perform?
 
-action: "GET" location: /students
+action: "GET" location: /students -- index
 
-action: "POST" location: /students
+action: "POST" location: /students -- update
 
-action: "GET" location: /students/new
+action: "GET" location: /students/new -- new
 
-action: "GET" location: /students/2
+action: "GET" location: /students/2 -- show
 
-action: "GET" location: /students/2/edit
+action: "GET" location: /students/2/edit -- edit
 
-action: "PATCH" location: /students/2
+action: "PATCH" location: /students/2 -- update
 
-action: "DELETE" location: /students/2
+action: "DELETE" location: /students/2 -- destroy
 
 5. As a developer, you are making an application to manage your to do list. Create 10 user stories that will help you get your application started. [Here is a resource](https://www.atlassian.com/agile/project-management/user-stories) to help you create your user stories.
+
+Story 1: Interaction with fish application, as a user of the application, I need to see a list of fish posts.
+    - Create a new rails application called 'best__spear_fishing_locations_in_okinawa'
+    - Create a resource for fish that has the follow information: name, type of fish, length, weight, venomous, and poisonous.
+    - Has a controller for fish, and has a action for index.
+    - Has a route and view for index.
+    - Can create a new fish. Can navigate back to landing page.
+    - Can update a fish. Can navigate back to landing page.
+    - Can delete a fish. Can navigate back to landing page.
+    - Can edit a fish. Can navigate back to landing page.
+    - Can see minimum of 3 fish on on the landing page.
+    - Hint: Need strong params, also show, edit, update, destroy have parameters.
+
+Story 2: Interaction with fish application, as a user of the application, I need to see a list of the locations of the fish using migrations.
+    - Create a migration for location to add the following column information: coastline.
+    - Update the values for coastline.
+
+Story 3: Need to secure the fish post application, I need to ensure only valid entries into the database.
+    - Ensure all fish posts are non blank non nil values.
+    - Ensure all fish names and type of fish are unique.
+
+Story 4: Customize the page with CSS to make it look presentable. 
